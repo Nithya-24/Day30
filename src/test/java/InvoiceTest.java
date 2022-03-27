ed5ec61b8e363ca65a257ded591188e7194f00b8
Nithya-24 <nithyaramadoz@gmail.com>
2022-03-27 22:42:14 +0530

    Calculate Fare is done

---
+++ src/test/java/InvoiceTest.java
@@ -0,0 +1,34 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import day30.InvoiceGenerator;
+import day30.TotalFare;
+
+public class InvoiceTest {
+
+	InvoiceGenerator invoice;
+	@Before 
+	public void initialization() {
+		invoice = new InvoiceGenerator();
+	}
+	
+	/**
+	 * Ttest case to check the total fare.
+	 */
+	
+	@Test
+	public void testGenerateInvoice() {
+		
+		Assert.assertEquals(103, invoice.invoiceGenerator(new TotalFare(10, 3)), 0.0);
+	}
+	/**
+	 * Test case to check for minimum fare.
+	 * If the fare is less that Rs.5 then we our min fare will be Rs.5.
+	 */
+	@Test
+	public void testGenerateInvoice_getMin() {
+		
+		Assert.assertEquals(5, invoice.invoiceGenerator(new TotalFare(0.1, 1)), 0.0);
+	}
+}
